0be147980c5cb4d2580b2c364e015acd5c0d3e38
dtovar <programerbyte@gmail.com>
2025-08-23 23:38:39 -0400

    Agregndo cambios

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,29 +1,44 @@
+import { useState } from "react";
+import './ItemCounter.css';
+
 interface Props {
     name: string;
     quantity?: number;
 }
 
-const ItemCounter = ({name, quantity}: Props) => {
+const ItemCounter = ({name, quantity = 1}: Props) => {
+
+  const [count, setCount] = useState(quantity);
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  }
+
+  const handleSubtract = () => {
+    setCount(count -1);
+    if(count <= 0){
+      setCount(0);
+    }
+  }
 
   return (
-    <section
-        style = {{
-        display: 'flex',
-        justifyContent: 'center',
-        gap: 10,
-        marginTop: 20,
-    }}>
-        <span
-        style={{
-            fontSize: 20,
-            fontWeight: 'bold',
-        }}
-        >
+    <section className="item-row">
+        <span 
+          className="item-name"
+          style={{
+            color: count === 0 ? 'red' : 'black',
+            fontWeight: count === 0 ? 'bold' : 'normal'
+          }}
+          >
           {name}
         </span>
-        <button> +1</button>
-        <span>{quantity}</span>
-        <button>-1</button>
+        <button
+          onClick={handleAdd}
+        >+1</button>
+        <span>{count}</span>
+        <button
+          onClick={handleSubtract}
+        >-1</button>
     </section>
   )
 }
